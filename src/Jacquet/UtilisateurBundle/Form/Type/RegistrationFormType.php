e30f25d977108b479c5fc910d31a9a4f76cadbbf
thibs911 <thibs911@hotmail.com>
2014-04-23 20:53:46 +0200

    Modifications

--- src/Jacquet/UtilisateurBundle/Form/Type/RegistrationFormType.php
+++ src/Jacquet/UtilisateurBundle/Form/Type/RegistrationFormType.php
@@ -14,47 +14,39 @@ namespace Jacquet\UtilisateurBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use FOS\UserBundle\Form\Type\RegistrationFormType as BaseType;
 
-class RegistrationFormType extends AbstractType
+class RegistrationFormType extends BaseType
 {
-    private $class;
-
-    /**
-     * @param string $class The User class name
-     */
-    public function __construct($class)
-    {
-        $this->class = $class;
-    }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        parent::buildForm($builder, $options);
         $builder
-            ->add('email', 'email', array('label' => 'email', 'translation_domain' => 'FOSUserBundle'))
-            ->add('username', null, array('label' => 'form.username', 'translation_domain' => 'FOSUserBundle'))
+            ->add('email', 'email', array('label' => 'Email', 'translation_domain' => 'FOSUserBundle'))
+            ->add('username', null, array('label' => 'Pseudo', 'translation_domain' => 'FOSUserBundle'))
             ->add('plainPassword', 'repeated', array(
                 'type' => 'password',
                 'options' => array('translation_domain' => 'FOSUserBundle'),
-                'first_options' => array('label' => 'form.password'),
-                'second_options' => array('label' => 'form.password_confirmation'),
-                'invalid_message' => 'fos_user.password.mismatch',
+                'first_options' => array('label' => 'Mot de Passe'),
+                'second_options' => array('label' => 'Confirmation du Mot de Passe'),
+                'invalid_message' => 'Mots de passe différent'
             ))
-            ->add('adresse','text', array('label' => 'adresse'))
-            ->add('nom', 'text', array('label' => 'nom'))
+            ->add('nom', 'text', array('label' => 'Nom'))
+            ->add('prenom', 'text', array('label' => 'Prénom'))
+            ->add('adresse', 'text', array('label' => 'Adresse'))
+            ->add('ville', 'text', array('label' => 'Ville'))
+            ->add('cp', 'integer', array('label' => 'Code Postal'))
+            ->add('telephone', 'text', array('label' => 'Téléphone'))
+            ->add('birthdate', 'datetime', array('label' => 'Date de Naissance',
+                                                  'widget' => 'single_text'))
 
         ;
-    }
 
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
-        $resolver->setDefaults(array(
-            'data_class' => $this->class,
-            'intention'  => 'registration',
-        ));
-    }
+        }
 
     public function getName()
     {
-        return 'fos_user_registration';
+        return 'jacquet_user_registration';
     }
 }
